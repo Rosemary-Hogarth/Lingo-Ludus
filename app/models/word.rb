9423271b144b3e87209454ce88dcd58f333907ce
Rosemary Hogarth <rohogarth@gmail.com>
2024-06-20 17:26:53 +0200

    Altered dashboard index and dashboard view

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,5 @@
 class Word < ApplicationRecord
   belongs_to :category
-  has_many :rounds
 
   validates :name, presence: true
   validates :level, presence: true, inclusion: { in: 1..3, message: "must be between 1 and 3" }
