2384cc1c73a8efb6647e47f39621213f9605f42c
Rosemary Hogarth <rohogarth@gmail.com>
2024-06-22 10:10:37 +0200

    Ready to push code

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,19 +3,18 @@ class DashboardsController < ApplicationController
 
   def index
     @user = current_user # Retrieves the current logged-in user
-    # start_date = 1.month.ago.beginning_of_hour
-    # end_date = Time.now.end_of_day
+    start_date = 1.month.ago.beginning_of_hour
+    end_date = Time.now.end_of_day
 
     # Filters games to only include those played within the last week.
-    # @games = @user.games.where(date: start_date..end_date)
+    @games = @user.games.where(date: start_date..end_date)
     # Debugging: Check if any games are retrieved
     Rails.logger.debug "Games in the last week: #{@games.inspect}"
 
     # Calculates the average score over the last week, grouped by day
     # @average_scores_over_time = @games.group_by_day(:date).average(:score).transform_values(&:to_f)
 
-    # Debugging: Check the average scores over time
-    Rails.logger.debug "Average scores over time: #{@average_scores_over_time.inspect}"
+
 
     # Calculates the total score dynamically by summing the score column from games associated with the current user (@user)
     # .sum --> active record method
@@ -25,6 +24,8 @@ class DashboardsController < ApplicationController
     # .order --> active record method
     @recent_games = @user.games.order(end_time: :desc).limit(5)
 
+    # Debugging: Check the average scores over time
+    Rails.logger.debug "Recent games: #{@recent_games.inspect}"
     # Calculates the average score of all game details associated with the user
     # .average --> active record method
     @average_score = @user.games.average(:score).to_f
