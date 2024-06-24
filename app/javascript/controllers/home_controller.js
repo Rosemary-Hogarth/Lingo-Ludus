2f18f787608f60d96d016b10d86dafba8152c43a
Rosemary Hogarth <rohogarth@gmail.com>
2024-06-24 15:34:00 +0200

    Changed home-controller js

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -3,12 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="home"
 export default class extends Controller {
   static targets = ["container", "shapes"];
+
+  id = null;
+  pos = 0;
+
   connect() {
       // Check if the current page is the homepage
       if (window.location.pathname === "/") {
         this.hideNavbar();
       }
-      this.generateSquares();
+      this.myMove();
   }
 
 
@@ -20,14 +24,36 @@ export default class extends Controller {
   }
 
 
-  // function to create and add squares
-    generateSquares = () => {
-      const shapes = this.shapesTarget;
+  myMove = () => {
+    const shapes = this.shapesTarget;
+    if (!shapes) return;
 
-      for (let i = 0; i < 10; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        shapes.appendChild(square);
-      }
-    };
+    clearInterval(id);
+    this.id = setInterval(this.frame.bind(this), 10);
   }
+
+  frame() {
+    const shapes = this.shapesTarget;
+      if (this.pos == 350) {
+        clearInterval(this.id);
+      } else {
+        this.pos++;
+        shapes.style.top = this.pos + 'px';
+        shapes.style.left = this.pos + 'px';
+      }
+  }
+}
+
+
+
+
+//  // function to create and add squares
+//  generateSquares = () => {
+//   const shapes = this.shapesTarget;
+
+//   for (let i = 0; i < 10; i++) {
+//     const square = document.createElement('div');
+//     square.classList.add('square');
+//     shapes.appendChild(square);
+//   }
+// };
