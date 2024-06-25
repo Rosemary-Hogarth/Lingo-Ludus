08abdbb9895c008f773ecca4a70d993c989d895d
Rosemary Hogarth <rohogarth@gmail.com>
2024-06-25 19:30:41 +0200

    Improved fade effect

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -38,7 +38,8 @@ export default class extends Controller {
     });
 
         // Redirect to games page after the animation finishes
-        const homepageDuration = this.shapesTargets.length * 200 + 5300; // (last index delay + animation duration)
+        const homepageDuration = this.shapesTargets.length * 200 + 5300;
+        console.log(homepageDuration) // (last index delay + animation duration)
         setTimeout(() => {
           this.fadeOutAndRedirect();
         }, homepageDuration);
