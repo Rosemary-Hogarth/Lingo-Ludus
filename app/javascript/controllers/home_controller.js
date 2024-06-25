210ce168878de4a25fe6979add54ba22984a85d3
Rosemary Hogarth <rohogarth@gmail.com>
2024-06-25 10:19:11 +0200

    Added animation and fade effect

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="home"
 export default class extends Controller {
-  static targets = ["shapes"];
+  static targets = ["shapes", "container"];
 
 
   connect() {
@@ -24,8 +24,10 @@ export default class extends Controller {
 
   animateSquares() {
     this.shapesTargets.forEach((shape, index) => {
-      // Set initial state outside the viewport
+      console.log(`Shape: ${shape}, Index: ${index}`);
+      // Start outside the viewport
       shape.style.transform = index % 2 === 0 ? 'translateX(-100vw)' : 'translateX(100vw)';
+      // start fast and then slow down
       shape.style.transition = 'transform 5s cubic-bezier(0.25, 1, 0.5, 1)';
 
 
@@ -34,18 +36,21 @@ export default class extends Controller {
         shape.style.transform = 'translateX(0)';
       }, index * 200); // stagger the animation
     });
+
+        // Redirect to games page after the animation finishes
+        const homepageDuration = this.shapesTargets.length * 200 + 3500; // (last index delay + animation duration)
+        setTimeout(() => {
+          this.fadeOutAndRedirect();
+        }, homepageDuration);
+      }
+
+    fadeOutAndRedirect() {
+      const container = this.containerTarget;
+      console.log(this.containerTarget)
+
+      container.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = "/play";
+      }, 1000);
+    }
   }
-}
-
-
-
-//  // function to create and add squares
-//  generateSquares = () => {
-//   const shapes = this.shapesTarget;
-
-//   for (let i = 0; i < 10; i++) {
-//     const square = document.createElement('div');
-//     square.classList.add('square');
-//     shapes.appendChild(square);
-//   }
-// };
