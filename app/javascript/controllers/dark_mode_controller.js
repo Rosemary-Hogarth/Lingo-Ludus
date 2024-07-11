cd88b8f0de5bab0c9462fe44f58b0fddf9094807
Rosemary Hogarth <rohogarth@gmail.com>
2024-07-11 21:10:52 +0200

    heroku

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -2,26 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dark-mode"
 export default class extends Controller {
   static targets = [
-    "body",
     "checkbox",
   ];
   connect() {
     console.log("dark mode connected");
     this.checkDarkMode();
+    console.log(this.element)
   }
   toggleDarkMode() {
     console.log("dark mode toggled");
-    const isDarkMode = this.bodyTarget.classList.toggle("dark-mode");
+    const isDarkMode = this.element.classList.toggle("dark-mode");
     localStorage.setItem("darkMode", isDarkMode);
     document.body.dispatchEvent(new CustomEvent('darkModeChanged', { detail: { isDarkMode: isDarkMode } }));
   }
   checkDarkMode() {
     const isDarkMode = localStorage.getItem("darkMode") === "true";
     if (isDarkMode) {
-      this.bodyTarget.classList.add("dark-mode")
+      this.element.classList.add("dark-mode")
       this.checkboxTarget.checked = true
     } else {
-      this.bodyTarget.classList.remove("dark-mode")
+      this.element.classList.remove("dark-mode")
     }
    }
 }
